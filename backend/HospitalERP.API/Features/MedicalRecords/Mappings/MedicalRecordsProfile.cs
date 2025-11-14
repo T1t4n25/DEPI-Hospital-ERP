2d250e0252b83c3b18380fc5022b826ec10870d5
Zeyad Hemeda <shadodiss@gmail.com>
2025-11-14 22:38:14 +0200

    feat: Complete MedicalRecords API implementation
    - Add AutoMapper profile with patient, doctor, and diagnosis mapping
    - Implement full CRUD controller endpoints
    - Add role-based authorization (Admin, Doctor)

--- backend/HospitalERP.API/Features/MedicalRecords/Mappings/MedicalRecordsProfile.cs
+++ backend/HospitalERP.API/Features/MedicalRecords/Mappings/MedicalRecordsProfile.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using HospitalERP.API.Features.MedicalRecords.Dtos;
+using HospitalERP.API.Models.Entities;
 
 namespace HospitalERP.API.Features.MedicalRecords.Mappings;
 
@@ -6,7 +8,16 @@ public class MedicalRecordsProfile : Profile
 {
     public MedicalRecordsProfile()
     {
-        // Mappings will be added here as DTOs and entities are created
+        CreateMap<CreateMedicalRecordDto, MedicalRecord>();
+        CreateMap<UpdateMedicalRecordDto, MedicalRecord>();
+        CreateMap<MedicalRecord, MedicalRecordListDto>()
+            .ForMember(dest => dest.PatientName, opt => opt.MapFrom(src => $"{src.Patient.FirstName} {src.Patient.LastName}"))
+            .ForMember(dest => dest.DoctorName, opt => opt.MapFrom(src => $"{src.Doctor.FirstName} {src.Doctor.LastName}"))
+            .ForMember(dest => dest.Diagnosis, opt => opt.MapFrom(src => src.Diagnosis.Diagnoses));
+        CreateMap<MedicalRecord, MedicalRecordDetailDto>()
+            .ForMember(dest => dest.PatientName, opt => opt.MapFrom(src => $"{src.Patient.FirstName} {src.Patient.LastName}"))
+            .ForMember(dest => dest.DoctorName, opt => opt.MapFrom(src => $"{src.Doctor.FirstName} {src.Doctor.LastName}"))
+            .ForMember(dest => dest.Diagnosis, opt => opt.MapFrom(src => src.Diagnosis.Diagnoses));
     }
 }
 
--- backend/HospitalERP.API/Features/MedicalRecords/MedicalRecordsController.cs
+++ backend/HospitalERP.API/Features/MedicalRecords/MedicalRecordsController.cs
@@ -1,3 +1,7 @@
+using HospitalERP.API.Common.Pagination;
+using HospitalERP.API.Features.MedicalRecords.Commands;
+using HospitalERP.API.Features.MedicalRecords.Dtos;
+using HospitalERP.API.Features.MedicalRecords.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +20,41 @@ public class MedicalRecordsController : ControllerBase
         _mediator = mediator;
     }
 
-    // Endpoints will be added here as Commands/Queries are created
+    [HttpGet]
+    public async Task<ActionResult<PaginatedResponse<MedicalRecordListDto>>> GetAll([FromQuery] QueryParams queryParams)
+    {
+        var result = await _mediator.Send(new GetAllMedicalRecordsQuery(queryParams));
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<MedicalRecordDetailDto>> GetById(int id)
+    {
+        var result = await _mediator.Send(new GetMedicalRecordByIdQuery(id));
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<MedicalRecordDetailDto>> Create([FromBody] CreateMedicalRecordDto dto)
+    {
+        var result = await _mediator.Send(new CreateMedicalRecordCommand(dto));
+        return CreatedAtAction(nameof(GetById), new { id = result.RecordID }, result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<MedicalRecordDetailDto>> Update(int id, [FromBody] UpdateMedicalRecordDto dto)
+    {
+        var command = new UpdateMedicalRecordCommand(dto with { RecordID = id });
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _mediator.Send(new DeleteMedicalRecordCommand(id));
+        return NoContent();
+    }
 }
 
