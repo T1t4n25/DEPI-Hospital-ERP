60309d51fb00f45737fd43f6d426b8b71e59d31b
Zeyad Hemeda <shadodiss@gmail.com>
2025-11-14 22:37:59 +0200

    feat: Complete Employees API implementation
    - Add AutoMapper profile with role, department, and gender mapping
    - Implement full CRUD controller endpoints
    - Add Admin role authorization

--- backend/HospitalERP.API/Features/Employees/Mappings/EmployeesProfile.cs
+++ backend/HospitalERP.API/Features/Employees/Mappings/EmployeesProfile.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using HospitalERP.API.Features.Employees.Dtos;
+using HospitalERP.API.Models.Entities;
 
 namespace HospitalERP.API.Features.Employees.Mappings;
 
@@ -6,7 +8,15 @@ public class EmployeesProfile : Profile
 {
     public EmployeesProfile()
     {
-        // Mappings will be added here as DTOs and entities are created
+        CreateMap<CreateEmployeeDto, Employee>();
+        CreateMap<UpdateEmployeeDto, Employee>();
+        CreateMap<Employee, EmployeeListDto>()
+            .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role.RoleName))
+            .ForMember(dest => dest.DepartmentName, opt => opt.MapFrom(src => src.Department.DepartmentName));
+        CreateMap<Employee, EmployeeDetailDto>()
+            .ForMember(dest => dest.GenderName, opt => opt.MapFrom(src => src.Gender.GenderName))
+            .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role.RoleName))
+            .ForMember(dest => dest.DepartmentName, opt => opt.MapFrom(src => src.Department.DepartmentName));
     }
 }
 
